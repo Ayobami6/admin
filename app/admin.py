f4b1d89aa1fce815c333ea736a232a917839de94
Ayobami6 <ayobamidele006@gmail.com>
2025-05-16 11:24:46 +0100

    Register Token and Utils models in the Django admin with custom admin classes

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from .models import Token, Utils
 
 # Register your models here.
+
+
+class TokenAdmin(admin.ModelAdmin):
+    list_display = ("token", "created_at", "ip_address")
+    search_fields = ("token", "")
+    list_filter = ("created_at",)
+    readonly_fields = ("created_at",)
+
+
+class UtilsAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "ai_apikey",
+    )
+    search_fields = ("name", "ai_apikey")
+
+
+admin.site.register(Token, TokenAdmin)
+admin.site.register(Utils, UtilsAdmin)
