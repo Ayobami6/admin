fcbdc1a52d19b041180b6643354c31e4d698f223
Ayobami6 <ayobamidele006@gmail.com>
2025-05-16 15:30:34 +0100

    Add root view and URL pattern for the API

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from sparky_utils.response import service_response
 
 # Create your views here.
+
+
+class Root(APIView):
+
+    def get(self, request, *args, **kwargs):
+        return service_response(
+            data={}, status="success", status_code=200, message="Welcome"
+        )
